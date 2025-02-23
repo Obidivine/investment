e756c33e6da51cfb470a9aae25a12b178733c6e0
Obidivine <123864887+Obidivine@users.noreply.github.com>
2025-02-23 13:29:04 +0100

    Update account_success.js

--- html/js/account_success.js
+++ html/js/account_success.js
@@ -1,1 +1,12 @@
-h
+document.addEventListener("DOMContentLoaded", function () { const username = localStorage.getItem("username"); const usernameSpan = document.getElementById("username");
+
+if (username) {
+    usernameSpan.textContent = username;
+} else {
+    usernameSpan.textContent = "User";
+}
+
+});
+
+function goToDashboard() { window.location.href = "dashboard.html"; }
+
